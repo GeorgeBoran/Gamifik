222b37c34d8c8be69179f0b9c6edf6c96a39dadc
George <73123066+GeorgeBoran@users.noreply.github.com>
2022-02-04 17:09:13 +0100

    ventana-formulario

--- src/app/ventana-formulario/ventana-formulario.component.ts
+++ src/app/ventana-formulario/ventana-formulario.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ventana-formulario.component.css'],
 })
 export class VentanaFormularioComponent implements OnInit {
+  progreso: number = 20;
+
   constructor() {}
 
   ngOnInit(): void {}
