169ad4557e4cb7550f6d43cce0f4e371091760c1
JhonnyBGs <73123155+JhonnyBGs@users.noreply.github.com>
2022-02-08 20:08:11 +0100

    Back V1

--- src/app/registrar/register/register.component.ts
+++ src/app/registrar/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  progreso: number = 20;
+  constructor () { }
 
-  constructor() {}
+  progreso: number = 50;
+  
+  mail: string = '';
+  img: string = '';
+  username: string = '';
+  name: string = '';
+  cognom: string = '';
+  password: string = '';
+  repassword: string = '';
+  centro: string = '';
+
+  displays: string[] = [' ','none','none','none'];
+  
+ 
+
+  next ()
+  {
+    if ( this.displays[ 0 ] == ' ' )
+    {
+      
+        this.displays[ 0 ] = 'none';
+        this.displays[ 1 ] = ' ';
+      
+    } else if(this.displays[ 1 ] == ' ')
+    {
+      
+        this.displays[ 1 ] = 'none';
+        this.displays[ 2 ] = ' ';
+      
+    }else if(this.displays[ 2 ] == ' ')
+    {
+      this.displays[ 3 ] = '';
+      this.displays[ 2 ] = 'none';
+    }else if(this.displays[ 3 ] == ' ')
+    {
+      this.displays[ 0 ] = '';
+      this.displays[ 3 ] = 'none';
+    }
+    
+  }
 
   ngOnInit(): void {}
 }
