9af3260a9e206fee6ed3b394424e1ba6541c10b3
JhonnyBGs <73123155+JhonnyBGs@users.noreply.github.com>
2022-02-09 20:03:33 +0100

    Back V2

--- src/app/registrar/register/register.component.ts
+++ src/app/registrar/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -10,30 +12,100 @@ export class RegisterComponent implements OnInit {
   progreso: number = 50;
 
   mail: string = '';
+
   img: string = '';
   username: string = '';
+
   name: string = '';
   cognom: string = '';
+
   password: string = '';
   repassword: string = '';
+
   centro: string = '';
+  fecha: string = '';
+  condicion: boolean = false;
+
+  message: string = '';
 
   displays: string[] = [' ', 'none', 'none', 'none','none'];
 
   next() {
-    if (this.displays[0] == ' ') {
-      this.displays[0] = 'none';
-      this.displays[1] = ' ';
-    } else if (this.displays[1] == ' ') {
-      this.displays[1] = 'none';
-      this.displays[2] = ' ';
-    } else if (this.displays[2] == ' ') {
-      this.displays[3] = '';
-      this.displays[2] = 'none';
+    if ( this.displays[ 0 ] == ' ' )
+    {
+      if ( this.comprobarEmail() )
+      {
+        this.displays[0] = 'none';
+        this.displays[1] = ' ';
+      } else
+      {
+        swal.fire( 'Email no valido!' );
+      }
+    } else if ( this.displays[ 1 ] == ' ' )
+    {
+      if ( this.username == '' )
+      {
+        swal.fire( 'Username no valido!' );
+      } else
+      {
+        this.displays[1] = 'none';
+        this.displays[2] = ' ';
+      }
+      
+    } else if ( this.displays[ 2 ] == ' ' )
+    {
+      if (this.comprobarPassword())
+      {
+        this.displays[3] = '';
+        this.displays[2] = 'none';
+      } else
+      {
+        swal.fire( this.message );
+      }
+      
     } else if (this.displays[3] == ' ') {
-      this.displays[0] = '';
+      this.displays[4] = '';
       this.displays[3] = 'none';
     }
+    else if (this.displays[3] == ' ') {
+      this.displays[0] = '';
+      this.displays[4] = 'none';
+    }
+  }
+
+  comprobarPassword ()
+  {
+    if ( this.password == " " )
+    {
+      this.message = "Debe introducir el password!";
+      return false;
+      
+    } else if ( this.repassword ==" ")
+      {
+        this.message = "Debe confirmar el password!";
+        return false;
+      }
+    else if(this.password==this.repassword)
+    {
+      return true;
+    } else
+    {
+      this.message = "El Password no coincide!";
+        return false;
+    }
+  }
+
+  comprobarEmail ()
+  {
+   
+    for (let index = 0; index < this.mail.length; index++) {
+      const element = this.mail[ index ];
+      if (element=='@')
+      {
+        return true;
+      }
+    }
+    return false;
   }
 
   ngOnInit(): void {}
