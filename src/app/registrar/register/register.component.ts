f2e8d2ff50390fd835254f79de02d4bf5c32ddf3
George <73123066+GeorgeBoran@users.noreply.github.com>
2022-02-16 19:58:31 +0100

    CSS

--- src/app/registrar/register/register.component.ts
+++ src/app/registrar/register/register.component.ts
@@ -9,7 +9,7 @@ import swal from 'sweetalert2';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  constructor(private sanitizer: DomSanitizer, private router:Router) {}
+  constructor(private sanitizer: DomSanitizer, private router: Router) {}
 
   progreso: number = 0;
 
@@ -28,227 +28,183 @@ export class RegisterComponent implements OnInit {
   centro: string = '';
   fecha: string = '';
 
-  inputIMG: HTMLElement = document.getElementById( 'inputIMG' ) as HTMLElement; 
+  inputIMG: HTMLElement = document.getElementById('inputIMG') as HTMLElement;
 
   alumno: boolean = true;
   message: string = '';
-  displays: string[] = [ ' ', 'none', 'none', 'none', 'none' ];
+  displays: string[] = [' ', 'none', 'none', 'none', 'none'];
 
-  showPass()
-  {
-    if ( this.showPassword == 'password' )
-    {
-      this.showPassword = "text";
-    } else
-    {
-      this.showPassword = "password";
+  showPass() {
+    if (this.showPassword == 'password') {
+      this.showPassword = 'text';
+    } else {
+      this.showPassword = 'password';
     }
   }
 
   next() {
-    if ( this.displays[ 0 ] == ' ' )
-    {
-      if ( this.comprobarEmail() )
-      {
+    if (this.displays[0] == ' ') {
+      if (this.comprobarEmail()) {
         this.progreso = 20;
         this.displays[0] = 'none';
         this.displays[1] = ' ';
-      } else
-      {
-        swal.fire( this.message );
+      } else {
+        swal.fire(this.message);
       }
-    } else if ( this.displays[ 1 ] == ' ' )
-    {
-      if ( this.comprobarUsername() )
-      {
-        if ( this.img == '../../assets/avatar.jpg' )
-        {
-            swal.fire({
-            title: 'Estas seguro que no quieres subir una foto de perfil?',
-            text: "Para subir una foto de perfil, haz click sobre el icono de usuario.",
-            icon: 'warning',
-            showCancelButton: true,
-            cancelButtonColor: '#d33',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'Confirmar'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.progreso = 50;
-              this.displays[1] = 'none';
-              this.displays[2] = ' ';
-            }
-          })
-          
-        } else
-        {
-              this.progreso = 40;
-              this.displays[1] = 'none';
-              this.displays[2] = ' ';
+    } else if (this.displays[1] == ' ') {
+      if (this.comprobarUsername()) {
+        if (this.img == '../../assets/avatar.jpg') {
+          swal
+            .fire({
+              title: 'Estas seguro que no quieres subir una foto de perfil?',
+              text: 'Para subir una foto de perfil, haz click sobre el icono de usuario.',
+              icon: 'warning',
+              showCancelButton: true,
+              cancelButtonColor: '#d33',
+              confirmButtonColor: '#3085d6',
+              confirmButtonText: 'Confirmar',
+            })
+            .then((result) => {
+              if (result.isConfirmed) {
+                this.progreso = 50;
+                this.displays[1] = 'none';
+                this.displays[2] = ' ';
+              }
+            });
+        } else {
+          this.progreso = 40;
+          this.displays[1] = 'none';
+          this.displays[2] = ' ';
         }
-        
-      } else 
-      {
-        swal.fire( this.message );
-      } 
-      
-    } else if ( this.displays[ 2 ] == ' ' )
-    {
-      if (this.comprobarPassword())
-      {
+      } else {
+        swal.fire(this.message);
+      }
+    } else if (this.displays[2] == ' ') {
+      if (this.comprobarPassword()) {
         this.progreso = 60;
         this.displays[3] = ' ';
         this.displays[2] = 'none';
-      } else
-      {
-        swal.fire( this.message );
+      } else {
+        swal.fire(this.message);
       }
-      
-    } else if ( this.displays[ 3 ] == ' ' )
-    {
-      if ( this.name == '' )
-      {
-        swal.fire( 'Introducir Nombre!' );
-      } else if ( this.cognom == '' )
-      {
-        swal.fire( 'Introducir Apellido!' );
-      } else
-      {
+    } else if (this.displays[3] == ' ') {
+      if (this.name == '') {
+        swal.fire('Introducir Nombre!');
+      } else if (this.cognom == '') {
+        swal.fire('Introducir Apellido!');
+      } else {
         this.progreso = 80;
         this.displays[4] = ' ';
         this.displays[3] = 'none';
       }
-    }
-    else if ( this.displays[ 4 ] == ' ' )
-    {
-      if ( this.alumno&&this.centro == '' )
-      {
-        swal.fire( 'Introducir nombre del centro!' );
-      } else if ( !this.alumno&&this.fecha == '' )
-      {
-        swal.fire( 'Introducir fecha de nacimiento!' );
-      } else
-      {
+    } else if (this.displays[4] == ' ') {
+      if (this.alumno && this.centro == '') {
+        swal.fire('Introducir nombre del centro!');
+      } else if (!this.alumno && this.fecha == '') {
+        swal.fire('Introducir fecha de nacimiento!');
+      } else {
         this.progreso = 100;
         this.registrarUsuario();
       }
-      
     }
   }
 
-  imgClick ()
-  {
-    this.inputIMG = document.getElementById( 'inputIMG' ) as HTMLElement; 
+  imgClick() {
+    this.inputIMG = document.getElementById('inputIMG') as HTMLElement;
     this.inputIMG.click();
   }
 
-  capturarFile(event:any): any {
-    const archivoCapturado = event.target.files[ 0 ];
+  capturarFile(event: any): any {
+    const archivoCapturado = event.target.files[0];
     this.extraerBase64(archivoCapturado).then((imagen: any) => {
       this.img = imagen.base;
-    })
+    });
   }
 
-  extraerBase64 = async ($event: any) => new Promise((resolve, reject):any => {
-    try {
-      const unsafeImg = window.URL.createObjectURL($event);
-      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
-      const reader = new FileReader();
-      reader.readAsDataURL($event);
-      reader.onload = () => {
-        resolve({
-          base: reader.result
-        });
-      };
-      reader.onerror = error => {
-        resolve({
-          base: null
-        });
-      };
+  extraerBase64 = async ($event: any) =>
+    new Promise((resolve, reject): any => {
+      try {
+        const unsafeImg = window.URL.createObjectURL($event);
+        const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+        const reader = new FileReader();
+        reader.readAsDataURL($event);
+        reader.onload = () => {
+          resolve({
+            base: reader.result,
+          });
+        };
+        reader.onerror = (error) => {
+          resolve({
+            base: null,
+          });
+        };
+      } catch (e) {
+        return null;
+      }
+    });
 
-    } catch (e) {
-      return null;
-    }
-  })
-
-  registrarUsuario ()
-  {
-    swal.fire({
-            title: 'Usuario registrado!',
-            icon: 'warning',
-            showCancelButton: false,
-            cancelButtonColor: '#d33',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'OK'
-          }).then((result) => {
-            if (result.isConfirmed) {
-              this.router.navigate([ '/pagina' ]);
-            }
-          })
+  registrarUsuario() {
+    swal
+      .fire({
+        title: 'Usuario registrado!',
+        icon: 'warning',
+        showCancelButton: false,
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'OK',
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          this.router.navigate(['/pagina']);
+        }
+      });
   }
 
-  selectType ()
-  {
-    if ( this.alumno )
-    {
+  selectType() {
+    if (this.alumno) {
       this.alumno = false;
-    } else
-    {
+    } else {
       this.alumno = true;
     }
   }
 
-  comprobarUsername ()
-  {
-    if ( this.username == '' )
-    {
-      this.message='Introducir username!'
+  comprobarUsername() {
+    if (this.username == '') {
+      this.message = 'Introducir username!';
       return false;
-    } else
-    {
+    } else {
       return true;
-    }
-    
-  }
-
-  comprobarPassword ()
-  {
-    if ( this.password == '' )
-    {
-      this.message = "Debe introducir el password!";
-      return false;
-      
-    } else if ( this.repassword =='')
-      {
-        this.message = "Debe confirmar el password!";
-        return false;
-      }
-    else if(this.password==this.repassword)
-    {
-      return true;
-    } else
-    {
-      this.message = "El Password no coincide!";
-        return false;
     }
   }
 
-  comprobarEmail ()
-  {
-    if ( this.mail == '' )
-    {
-      this.message='Introducir Email!'
+  comprobarPassword() {
+    if (this.password == '') {
+      this.message = 'Debe introducir el password!';
       return false;
-    } else
-    {
+    } else if (this.repassword == '') {
+      this.message = 'Debe confirmar el password!';
+      return false;
+    } else if (this.password == this.repassword) {
+      return true;
+    } else {
+      this.message = 'El Password no coincide!';
+      return false;
+    }
+  }
+
+  comprobarEmail() {
+    if (this.mail == '') {
+      this.message = 'Introducir Email!';
+      return false;
+    } else {
       for (let index = 0; index < this.mail.length; index++) {
-      const element = this.mail[ index ];
-      if (element=='@')
-      {
-        return true;
+        const element = this.mail[index];
+        if (element == '@') {
+          return true;
+        }
       }
     }
-    }
-    this.message='Email no valido!'
+    this.message = 'Email no valido!';
     return false;
   }
 
