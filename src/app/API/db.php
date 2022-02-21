0dc47ca682a3e7c31558f2a2bf4cc9cfbddcb017
EmanuelAlexPrut <73123038+EmanuelAlexPrut@users.noreply.github.com>
2022-02-21 20:35:43 +0100

    Add files via upload

---
+++ src/app/API/db.php
@@ -0,0 +1,27 @@
+<?php
+class Conexion{
+    //Variables
+    private $servidor='localhost';
+    private $database='goat_gamifik';
+    private $usuario='root';
+    private $password='';
+    public $con;
+    function con()
+    {
+        $this->con = mysqli_connect($this->servidor, $this->usuario, $this->password, $this->database);
+    }
+    //Funcion comprobar la conexion
+    function comprobarConexion(){
+        if (!$this->con) {
+            echo "no se han validado los datos";
+        }else {
+            return false;
+        }
+    }
+    //Funcion retornar la conexion
+    function getConexion(){
+        return $this->con;
+    }
+}
+
+?>
---
+++ src/app/API/goat_gamifik.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-02-2022 a las 19:51:12
+-- Versión del servidor: 10.4.21-MariaDB
+-- Versión de PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `goat_gamifik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `email` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nickname` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `email` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `apellidos` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `nickname` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `centro` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
