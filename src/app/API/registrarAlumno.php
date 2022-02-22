dbf7c3adf7b4b4f0ba9cbeaba636eb04288755b9
EmanuelAlexPrut <73123038+EmanuelAlexPrut@users.noreply.github.com>
2022-02-22 17:05:52 +0100

    Update registrarAlumno.php

--- src/app/API/registrarAlumno.php
+++ src/app/API/registrarAlumno.php
@@ -1,37 +1,4 @@
-<<<<<<< Updated upstream
-<?php
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Content-Type: text/html; charset=UTF-8');
 
-
-global $datos;
-
-require("db.php"); // IMPORTA EL ARCHIVO CON LA CONEXION A LA DB
-
-
-$conexion = con(); // CREA LA CONEXION
-
-
-// REALIZA LA QUERY A LA DB
-$registros = mysqli_query($conexion, "INSERT INTO `alumnos`
-                         (`email`, `nombre`, `apellidos`, `nickname`, `fecha_nacimiento`, `password`, `id`)
-                         VALUES ('$_GET[email]',  '$_GET[nombre]', '$_GET[apellidos]', '$_GET[nickname]', '$_GET[fecha_nacimiento]' , '$_GET[password]' , '$_GET[id]', 'Clasico', 0, 6);");
-
-
-class Result {}
-
-// GENERA LOS DATOS DE RESPUESTA
-$response = new Result();
-$response->resultado = 'OK';
-$response->mensaje = 'EL USUARIO SE ELIMINO EXITOSAMENTE';
-
-header('Content-Type: application/json');
-
-echo json_encode($response); // MUESTRA EL JSON GENERADO
-
-?>
-=======
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
@@ -67,4 +34,3 @@ echo json_encode($registros); // MUESTRA EL JSON GENERADO
 
 ?>
 
->>>>>>> Stashed changes
