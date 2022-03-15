2c24cd454c3628e79d1b03ed73ee75c6655086d8
George <73123066+GeorgeBoran@users.noreply.github.com>
2022-03-15 20:14:24 +0100

    Ranking/Mejoras

--- src/app/rankings/visualizar-ranking/visualizar-ranking.component.ts
+++ src/app/rankings/visualizar-ranking/visualizar-ranking.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-visualizar-ranking',
   templateUrl: './visualizar-ranking.component.html',
-  styleUrls: ['./visualizar-ranking.component.css']
+  styleUrls: ['./visualizar-ranking.component.css'],
 })
 export class VisualizarRankingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  volverTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
   }
-
 }
